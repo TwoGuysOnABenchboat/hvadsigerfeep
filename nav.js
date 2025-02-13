80c13282d79fc9bb136523f62301feb54b2822c4
Fuzzymelox <fuzzymelox@hotmail.com>
2025-02-13 13:49:23 +0100

    Fixed case sensitive stuff

--- nav.js
+++ nav.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     let navbar = `
         <div class="nav">
-            <a href="/"><img src="Graphics/Tabs/home.png" alt="Home"></a>
-            <a href="/Choss/"><img src="Graphics/Tabs/choss.png" alt="Choss"></a>
-            <a href="/About/"><img src="Graphics/Tabs/about.png" alt="About"></a>
+            <a href="/"><img src="Graphics/Tabs/Home.png" alt="Home"></a>
+            <a href="/Choss/"><img src="Graphics/Tabs/Choss.png" alt="Choss"></a>
+            <a href="/About/"><img src="Graphics/Tabs/About.png" alt="About"></a>
         </div>
     `;
 
