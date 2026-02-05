e43e45fc0c7419b32146181e192e69ffdfdfe7f9
Casper <fuzzymelox@gmail.com>
2026-02-05 09:36:33 +0100

    Force readable font for blog post paragraphs

--- nav.js
+++ nav.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let pathPrefix = window.location.pathname.includes("/Choss") || window.location.pathname.includes("/About") ? "../" : "";
     let currentPath = window.location.pathname.toLowerCase();
 
     let navbar = `
-        <div class="nav-bar"></div> 
+        <div class="nav-bar"></div>
         <div class="nav">
-            ${currentPath !== "/" ? `<a href="/"><img src="${pathPrefix}Graphics/Tabs/home.png" alt="Home"></a>` : ""}
-            ${!currentPath.includes("/choss") ? `<a href="/Choss"><img src="${pathPrefix}Graphics/Tabs/choss.png" alt="Choss"></a>` : ""}
-            ${!currentPath.includes("/about") ? `<a href="/About"><img src="${pathPrefix}Graphics/Tabs/about.png" alt="About"></a>` : ""}
+            ${currentPath !== "/" ? `<a class="nav-link" href="/">Home</a>` : ""}
+            ${!currentPath.includes("/liminalbrutalis") ? `<a class="nav-link" href="/LiminalBrutalis">Liminal Brutalis</a>` : ""}
+            ${!currentPath.includes("/blog") ? `<a class="nav-link" href="/Blog">Blog</a>` : ""}
+            ${!currentPath.includes("/about") ? `<a class="nav-link" href="/About">About</a>` : ""}
         </div>
     `;
 
